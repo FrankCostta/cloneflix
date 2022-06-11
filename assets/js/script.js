1e8f7ff16400510311a7776f8e89b6955d8a327a
FrankCostta <fransta8@gmail.com>
2022-06-10 23:12:44 -0300

    Projeto original em uma nova Branch >> 'original'!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 const navbar = document.getElementById('navbar');
+const btnCheck = document.getElementById('checkbox');
 
 function animeScroll() {
 	const windowTop = window.scrollY;
-	console.log(windowTop);
-
+	
 	if (windowTop > 20) {
-		navbar.style.backgroundColor = "#141414";
+		navbar.style.backgroundColor = '#141414';
 	} else {
-		navbar.style.backgroundColor = "#0000"
+		navbar.style.backgroundColor = '#0000';
 	}
 }
 
 window.addEventListener('scroll', () => {
 	animeScroll();
 });
+
+
+btnCheck.addEventListener('click', () => {
+	const a = document.querySelectorAll('.menu a');
+	
+	if (btnCheck.checked) {
+		for(let i=0;i<a.length;i++) {
+			a[i].style.display = 'block';
+		}
+	} else {
+		for(let i=0;i<a.length;i++) {
+			a[i].style.display = 'none';
+		}
+	}
+})
